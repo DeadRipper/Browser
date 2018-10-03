a19f419c56b20502d8316f211408d5d2cb718415
DeadRipper <hardyextrim@gmail.com>
2018-10-03 20:26:59 +0300

    Add files via upload

---
+++ Browser/Browser/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace Browser
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            wbSample.Navigate("http://www.google.com");
+        }
+
+        private bool IsUrlValid(string url)
+        {
+            string reg = @"^(http|https|ftp|)\://|[a-zA-Z0-9\-\.]+\.[a-zA-Z](:[a-zA-Z0-9]*)?/?([a-zA-Z0-9\-\._\?\,\'/\\\+&amp;%\$#\=~])*[^\.\,\)\(\s]$^(.com|.ua|.ru|)";
+            Regex regex = new Regex(reg, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            return regex.IsMatch(txtUrl.Text);
+        }
+
+        private void txtUrl_KeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.Key == Key.Enter)
+                {
+                    //wbSample.Navigate("http://" + txtUrl.Text + ".com");
+                    if (IsUrlValid(txtUrl.Text) == true)
+                    {
+                        wbSample.Navigate(txtUrl.Text);
+                    }
+                    else
+                    {
+                        wbSample.Navigate("http://" + txtUrl.Text + ".com");
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                
+            }
+        }
+
+        private void wbSample_Navigating(object sender, System.Windows.Navigation.NavigatingCancelEventArgs e)
+        {
+            txtUrl.Text = e.Uri.OriginalString;
+        }
+
+        private void BrowseBack_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = ((wbSample != null) && (wbSample.CanGoBack));
+        }
+
+        private void BrowseBack_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            wbSample.GoBack();
+        }
+
+        private void BrowseForward_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = ((wbSample != null) && (wbSample.CanGoForward));
+        }
+
+        private void BrowseForward_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            wbSample.GoForward();
+        }
+
+        private void GoToPage_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void GoToPage_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            wbSample.Navigate(txtUrl.Text);
+        }
+
+    }
+}
